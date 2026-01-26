102de3482da48d58904b1ca09d3400ae3de078a3
MD MEHAR BELAL <mdmeharbelal2004@gmail.com>
2026-01-26 19:38:19 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
 
 
-dotenv.config({ 
-  path: './.env' 
+dotenv.config({
+    path: './.env'
 });
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Failed to connect to the database:", error);
+        process.exit(1);
+    });
 
 
 
